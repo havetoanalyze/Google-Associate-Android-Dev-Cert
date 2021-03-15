4ad446cfbe77b463c4e8f1780ba59e56e6a6f063
Sean <sean@oneals.org>
2021-03-14 23:56:25 -0500

    Setup dice roll.

--- DiceRoller/app/src/main/java/net/redhand/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/redhand/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package net.redhand.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { this.rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
