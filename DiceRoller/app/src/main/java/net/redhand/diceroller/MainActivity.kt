74f25f4fc292de331e91d397cae2be78388c170e
Sean <sean@oneals.org>
2021-03-16 00:22:13 -0500

    Finished dice roller app

--- DiceRoller/app/src/main/java/net/redhand/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/redhand/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package net.redhand.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,13 +15,26 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { this.rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
+        val diceImage: ImageView = findViewById(R.id.imageView)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
